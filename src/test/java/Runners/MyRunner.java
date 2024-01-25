f7927bf08a4abe1dd0f8a1595089a77a7bda1d4e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-01-25 22:06:04 +0530

    MyRunner.java

--- src/test/java/Runners/MyRunner.java
+++ src/test/java/Runners/MyRunner.java
@@ -1,5 +1,7 @@
 package Runners;
 
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
@@ -15,4 +17,11 @@ import io.cucumber.testng.CucumberOptions;
 		)
 public class MyRunner extends AbstractTestNGCucumberTests {
 
+	@DataProvider(parallel=true)
+	public Object[][] scenarios(){
+		return super.scenarios();
+	}
+	{
+		
+	}
 }
