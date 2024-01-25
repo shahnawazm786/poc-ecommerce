fe07c846d7cddb975262d68e9f900f6fc396bd36
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-01-25 21:34:35 +0530

    Reading data from excel

--- src/test/java/utils/Constant.java
+++ src/test/java/utils/Constant.java
@@ -4,6 +4,5 @@ public class Constant {
 	public static final String CHROME_LOCTION="src\\test\\resources\\drivers\\chrome-win64\\chrome.exe";
 	public static final String URL="https://www.chilternoakfurniture.co.uk";
 	public static final String PROPERTIES_FILE_NAME="config.properties";
-	
 
 }
--- src/test/java/utils/ReadDataFromExcel.java
+++ src/test/java/utils/ReadDataFromExcel.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -27,6 +28,44 @@ public class ReadDataFromExcel {
 		}
 		
 		return subscriber;
+	}
+	
+	public String getCellData() throws IOException{
+		String data="";
+		FileInputStream inputStream=new FileInputStream(new File("src/test/resources/excel/subscriber.xlsx"));
+		XSSFWorkbook workBook=new XSSFWorkbook(inputStream);
+		XSSFSheet sheet=workBook.getSheetAt(0);
+		for(Row row:sheet) {
+			for(Cell cell:row) {
+				switch(cell.getCellType()) {
+				case NUMERIC:
+					data=Double.toString(cell.getNumericCellValue());
+					break;
+				case STRING:
+					data=cell.getStringCellValue();
+					break;
+				case FORMULA:
+					data=cell.getCellFormula();
+					break;
+				case BLANK:
+					data="";
+					break;
+				case BOOLEAN:
+					data=Boolean.toString(cell.getBooleanCellValue());
+					break;
+				case ERROR:
+					data=Byte.toString(cell.getErrorCellValue());
+					break;
+				case _NONE:
+					data="";
+					break;
+				default:
+					data=cell.getStringCellValue();
+					break;
+				}
+			}
+		}
+		return data;
 	}
 
 }
